49ca374b9060a6340659682ce2ef3f6881a50d29
Amy <amyannnwong@gmail.com>
2022-08-23 01:35:30 -0400

    doughnut chart renders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import BarChart from './charts/BarChart';
+import DoughnutChart from './charts/DoughnutChart'
 
 function App() {
   return (
     <div className="App">
       <BarChart />
+      <br /><hr /><br />
+      <DoughnutChart />
     </div>
   );
 }
