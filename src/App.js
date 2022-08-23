77f66b11a4a69b1f743efaf4172baedd731ce9c8
Amy <amyannnwong@gmail.com>
2022-08-23 01:41:29 -0400

    pie chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import BarChart from './charts/BarChart';
 import DoughnutChart from './charts/DoughnutChart'
+import LineChart from './charts/LineChart';
+import PieChart from './charts/PieChart';
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <BarChart />
       <br /><hr /><br />
       <DoughnutChart />
+      <br /><hr /><br />
+      <LineChart />
+      <br /><hr /><br />
+      <PieChart />
     </div>
   );
 }
---
+++ src/charts/PieChart.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(Tooltip, Legend, ArcElement);
+
+const PieChart = () => {
+  // useState is used to create variables in React
+  const [chart, setChart] = useState([]); // chart is getter that gets whatever data we have in this application, setChart is setter
+
+  let baseURL = "https://api.coinranking.com/v2/coins/?limit=10"; // limit query paramater set to 10 so that it only fetches 10 data from api
+  let proxyURL = "https://cors-anywhere.herokuapp.com/";
+  let apiKey = "coinranking3fc70f2c70bcb7d63e8258d1372ff80b732e3f557f265672";
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      await fetch(`${proxyURL}${baseURL}`, {
+        // this is from coinranking documentation:
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": `${apiKey}`,
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then((response) => {
+          response.json().then((json) => {
+            console.log("json: \n", json);
+            setChart(json.data)
+          });
+        })
+        .catch((error) => {
+          console.log("error: \n", error);
+        });
+    };
+    fetchCoins();
+  }, [baseURL, proxyURL, apiKey]);
+
+  console.log('chart: \n', chart)
+
+  let data = {
+    labels: chart?.coins?.map(x => x.name),
+    datasets: [
+      {
+        label: `${chart?.coins?.length} Coins Available`,
+        data: chart?.coins?.map(x => x.price),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  let options = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    legend: {
+      labels: {
+        fontSize: 25,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Pie height={400} data={data} options={options} />
+    </div>
+  );
+};
+
+export default PieChart;
